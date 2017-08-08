8581ed05843fba3fa0d821e3f40daab7149431c6
Lenson Lee <litianming@chinamobile.com>
2017-08-08 11:23:04 +0800

    1）完成新建评估页面LoginInfo、TaskHisList的返回; 2）增加后评估Item项信息的插入。

---
+++ src/main/java/com/zq/vo/process/TaskHisVO.java
@@ -0,0 +1,74 @@
+package com.zq.vo.process;
+
+import java.util.*;
+import java.sql.Timestamp;
+
+public class TaskHisVO {
+
+	private String taskName;
+	private String assignee;
+	private Timestamp dealTime;
+	private String dealResult;
+	private String dealOpinion;
+
+	public String getTaskName() {
+		return this.taskName;
+	}
+
+	/**
+	 * 
+	 * @param taskName
+	 */
+	public void setTaskName(String taskName) {
+		this.taskName = taskName;
+	}
+
+	public String getAssignee() {
+		return this.assignee;
+	}
+
+	/**
+	 * 
+	 * @param assignee
+	 */
+	public void setAssignee(String assignee) {
+		this.assignee = assignee;
+	}
+
+	public Timestamp getDealTime() {
+		return this.dealTime;
+	}
+
+	/**
+	 * 
+	 * @param dealTime
+	 */
+	public void setDealTime(Timestamp dealTime) {
+		this.dealTime = dealTime;
+	}
+
+	public String getDealResult() {
+		return this.dealResult;
+	}
+
+	/**
+	 * 
+	 * @param dealResult
+	 */
+	public void setDealResult(String dealResult) {
+		this.dealResult = dealResult;
+	}
+
+	public String getDealOpinion() {
+		return this.dealOpinion;
+	}
+
+	/**
+	 * 
+	 * @param dealOpinion
+	 */
+	public void setDealOpinion(String dealOpinion) {
+		this.dealOpinion = dealOpinion;
+	}
+
+}
